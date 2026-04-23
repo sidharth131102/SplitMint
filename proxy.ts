c77d7886815c736e3334817e222dad49dca6dcee
sidharth131102 <sidharthunnikrishnan2002@gmail.com>
2026-04-23 20:02:55 +0530

    Fix proxy to use NextAuth v5 auth, remove debug logging

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth.config";
 
-export const { auth: proxy } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+
+export const proxy = auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
